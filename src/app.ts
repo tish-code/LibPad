789e7b88a9e4d9bb818f88a91e171ce8aff177d4
tish-code <eliastish2003@gmail.com>
2022-11-28 16:40:23 +0100

    initiated database connection

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,22 @@
 import createError from "http-errors";
 import express, { NextFunction, Response, Request } from "express";
 import path from "path";
-import cookieParser from "cookie-parser";
-
+import { debug } from "www";
 import { IndexRouter } from "./routes/index";
 import { UserRouter } from "./routes/users";
+import { useDataBase } from "libs/db";
 
 export const app = express();
+
+// connect to db
+useDataBase()
+  .then(() => {
+    debug("Database connected successfully");
+  })
+  .catch((err) => {
+    debug(err);
+    debug("error connecting to db!");
+  });
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -14,20 +24,15 @@ app.set("view engine", "jade");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", IndexRouter);
 app.use("/users", UserRouter);
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  // render the error page
   res.status(err.status || 500);
   res.send({ message: err.message });
 });
