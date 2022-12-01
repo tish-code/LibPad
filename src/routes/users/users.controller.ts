878629cb22e9f2781b7a6bd6baf69441807bd793
tish-code <eliastish2003@gmail.com>
2022-12-01 15:52:37 +0100

    implemented user-login

--- src/routes/users/users.controller.ts
+++ src/routes/users/users.controller.ts
@@ -2,9 +2,19 @@ import { userService } from "./users.service";
 import express from "express";
 export const UserRouter = express.Router();
 
-UserRouter.post("/", async (req, res, next) => {
+UserRouter.post("/sign-up", async (req, res, next) => {
   try {
     const result = await userService.userSignUp(req.body);
+    res.send(result);
+  } catch (e) {
+    next(e);
+  }
+});
+
+UserRouter.post("/login", async (req, res, next) => {
+  try {
+    let { password, username } = req.body;
+    const result = await userService.UserLogin(username, password);
     res.send(result);
   } catch (e) {
     next(e);
